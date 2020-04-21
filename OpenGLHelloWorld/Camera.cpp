a6616099f52d65ab82baafc9c945bd8e6a7f3661
skyhuangdan <skyhuangdan@qq.com>
2020-04-21 17:51:05 +0800

    增加相机控制类

---
+++ OpenGLHelloWorld/Camera.cpp
@@ -0,0 +1,99 @@
+#include "Camera.h"
+
+
+
+
+Camera::Camera(
+	  glm::vec3 pos /*= glm::vec3(0.0f, 0.0f, 0.0f) */
+	, glm::vec3 up /*= glm::vec3(0.0f, 1.0f, 0.0f) */
+	, float yaw /*= YAW */
+	, float pitch /*= PITCH */)
+	: m_Position(pos)
+	, m_WorldUp(up)
+	, m_Yaw(yaw)
+	, m_Pitch(pitch)
+	, m_Front(glm::vec3(0.0f, 0.0f, -1.0f))
+	, m_MovementSpeed(SPEED)
+	, m_MouseSensitivity(SENSITIVITY)
+	, m_Zoom(ZOOM)
+{
+}
+
+Camera::~Camera()
+{
+}
+
+glm::mat4 Camera::GetViewMatrix()
+{
+	return glm::lookAt(m_Position, m_Position+ m_Front, m_Up);
+}
+
+void Camera::PrecessKeyBoard(int type, float deltatime)
+{
+	//速率
+	float velocity = m_MovementSpeed * deltatime;
+	switch (type)
+	{
+	case _FORWARD_:
+		m_Position += m_Front * velocity;
+		break;
+	case _BACKWARD_:
+		m_Position -= m_Front * velocity;
+		break;
+	case _LEFT_:
+		m_Position -= m_Right * velocity;
+		break;
+	case _RIGHT:
+		m_Position *= m_Right * velocity;
+		break;
+	}
+}
+
+void Camera::ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch)
+{
+	xoffset *= m_MouseSensitivity;
+	yoffset *= m_MouseSensitivity;
+
+	m_Yaw += xoffset;
+	m_Pitch += yoffset;
+
+	if (constrainPitch)
+	{
+		if (m_Pitch > 89.0f)
+			m_Pitch = 89.0f;
+		if (m_Pitch < -89.0f)
+			m_Pitch = -89.0f;
+	}
+
+	updateCameraVectors();
+}
+
+void Camera::updateCameraVectors()
+{
+	//重新计算 前向量 
+	glm::vec3 front;
+	front.x = cos(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
+	front.y = sin(glm::radians(m_Pitch));
+	front.z = cos(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
+	m_Front = glm::normalize(front);
+
+	//重新计算右向量 和 上向量
+
+	m_Right = glm::normalize(glm::cross(m_Front, m_WorldUp));
+	m_Up = glm::normalize(glm::cross(m_Right, front));
+	//标准化向量，因为他们的长度更接近于0，甚于你缓慢的上下看的结果。。。。翻译有点诡异
+	// Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+}
+
+
+void Camera::ProcessMouseScroll(float offset)
+{
+	if (m_Zoom >= 1.0f && m_Zoom <= 45.0f)
+		m_Zoom -= offset;
+
+	if (m_Zoom <= 1.0f)
+		m_Zoom = 1.0f;
+	if (m_Zoom >= 45.0f)
+		m_Zoom = 45.0f;
+
+}
---
+++ OpenGLHelloWorld/Camera.h
@@ -0,0 +1,59 @@
+#pragma once
+
+
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+
+
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+const float SPEED = 2.5f;
+const float SENSITIVITY = 0.1f;
+const float ZOOM = 45.0f;
+
+class Camera
+{
+public:
+	Camera( glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f)
+	, glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f)
+		, float yaw = YAW
+		, float pitch = PITCH
+		);
+	~Camera();
+
+
+	enum {
+		_FORWARD_,
+		_BACKWARD_,
+		_LEFT_,
+		_RIGHT,
+	};
+	// 获得lookat变换矩阵
+	glm::mat4 GetViewMatrix();
+	// 根据键盘操作移动镜头
+	void PrecessKeyBoard(int type, float deltatime);
+	//根据鼠标移动设置俯仰角以及偏航角
+	void ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch);
+
+	void ProcessMouseScroll(float offset);
+private:
+	void updateCameraVectors();
+	//相机属性
+	glm::vec3 m_Position;
+	glm::vec3 m_Front;
+	glm::vec3 m_Up;
+	glm::vec3 m_Right;
+	glm::vec3 m_WorldUp;
+
+	//欧拉角
+	float     m_Yaw;//偏航角
+	float     m_Pitch;//俯仰角
+	//相机参数
+	float     m_MovementSpeed;
+	float     m_MouseSensitivity;
+	float     m_Zoom;
+};
+
