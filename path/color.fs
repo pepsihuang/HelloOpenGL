c9e74875a80a992202612f04e23c59e0570fba41
skyhuangdan <skyhuangdan@qq.com>
2020-04-23 12:20:57 +0800

    基本光照的着色器

--- path/color.fs
+++ path/color.fs
@@ -1,10 +1,30 @@
 #version 330 core
 out vec4 FragColor;
+
+in vec3 Normal;
+in vec3 FragPos;
   
 uniform vec3 objectColor;
 uniform vec3 lightColor;
+uniform vec3 lightPos;
 
 void main()
 {
-    FragColor = vec4(lightColor * objectColor, 1.0);
+   
+    //FragColor = vec4(lightColor * objectColor, 1.0);
+
+
+
+	vec3 norm = normalize(Normal);//法向量的标准化
+	vec3 lightDir = normalize(lightPos - FragPos);//定向光线的标准化
+
+	float diff = max(dot(norm, lightDir), 0.0);//点乘 如果结果为负数,说明角度超过90°, 负数颜色是没有定义的.
+	vec3 diffuse = diff * lightColor;//从而根据角度而得到入射光线的过度
+
+	float ambientStrength = 0.1;//环境光
+	vec3 ambient = ambientStrength * lightColor;
+
+	vec3 result = (ambient + diffuse) * objectColor;
+    FragColor = vec4(result, 1.0);
+
 }
--- path/color.vs
+++ path/color.vs
@@ -1,13 +1,20 @@
 
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;//数据是一定先到顶点着色器,然后从顶点着色器传递给片段着色器
 
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
+out vec3 Normal;
+out vec3 FragPos;//片段的位置 疑问:从顶点着色器发送片段的位置,但顶点着色器在光栅化的前面完成. 那些片段的位置从哪来?
+
 void main()
 {
+	FragPos = vec3(model* vec4(aPos, 1.0));//将顶点坐标转换成世界坐标系
+	Normal = aNormal;
+
 	gl_Position = projection * view * model * vec4(aPos, 1.0f);
 }
